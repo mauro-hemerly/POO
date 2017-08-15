47dbc1212e3cdfa91feb57c5872374b1ba60fa52
mauro-hemerly <mauro.hemerly@gmail.com>
2017-08-15 14:10:14 -0300

    Classe Aluno atualizada

--- Projetos NetBeans/SegundoProjetoNB/src/Principal/TesteAluno.java
+++ Projetos NetBeans/SegundoProjetoNB/src/Principal/TesteAluno.java
@@ -1,5 +1,7 @@
 package Principal;
 
+import Classes.Aluno;     // ou: import Classes.*;
+
 /**
  *
  * @author Mauro Hemerly (Hämmerli) Gazzani <mauro.hemerly@gmail.com>
@@ -8,6 +10,13 @@ package Principal;
 // Classe Principal
 public class TesteAluno {   
     public static void main(String[] args) {
+        // Instanciando objeto do tipo Aluno.
+        // Neste caso, o aluno de nome 'Jose Carlos da Silva' e matrícula '190785'.
+        Aluno a1 = new Aluno(); // a1: identificação do obejto
+        
+        // Instanciando outro objeto do tipo Aluno.
+        // Neste caso, o aluno de nome 'Adriano Araujo' e matrícula '123456'.
+        Aluno a2 = new Aluno("Adriano Araujo","123456");   // a2: identificação do 2º objeto
         
     }
     
