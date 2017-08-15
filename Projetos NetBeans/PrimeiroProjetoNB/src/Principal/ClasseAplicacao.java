b143df43db42abfb49fdcd08744c15fccfe8fda0
mauro-hemerly <mauro.hemerly@gmail.com>
2017-08-15 13:16:29 -0300

    Primeiro Projeto em NetBeans com Java

---
+++ Projetos NetBeans/PrimeiroProjetoNB/src/Principal/ClasseAplicacao.java
@@ -0,0 +1,13 @@
+package Principal;
+
+/**
+ *
+ * @author Mauro Hemerly (Hämmerli) Gazzani <mauro.hemerly@gmail.com>
+ */
+public class ClasseAplicacao {
+   
+    public static void main(String[] args) {
+        System.out.println("Boa Noite Caros Alunos dos Cursos de EC e SI...");
+    }
+    
+}
